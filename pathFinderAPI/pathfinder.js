b218cb418f35cbea754770b456361083280ae44f
Kejie Jack He <kjhe217@gmail.com>
2020-06-23 21:47:48 -0400

    20200623 JackHKJ 3 Bug Fix
    1. Fixed the duplicate on setting start/end
    2. Fixed the color shown before actual animation appear on the visited element
    3. fixed the animation of the path element, add delay on the path execution

--- pathFinderAPI/pathfinder.js
+++ pathFinderAPI/pathfinder.js
@@ -16,10 +16,14 @@ for (let i = 0; i < GRID_ROW_SIZE; i++) {
     Grid.push([]);    
     for (let j = 0; j < GRID_COL_SIZE; j++) {
         Grid[i].push({id:i+"-"+j,row:i, col:j, State:"NULL", VisitedAt:-1  })        
+        /**
+         * the node element is used as a container for the element
+         * the inner div element is used to display the animation
+         */
     }
 }
 Grid[0][0].State = "Start"
-Grid[8][8].State = "End"
+Grid[GRID_ROW_SIZE -1][GRID_COL_SIZE-1].State = "End"
 // for (let i = 0; i < GRID_ROW_SIZE; i++) {    
     // for (let j = 0; j < GRID_COL_SIZE; j++) {
 //         console.log(Grid[i][j].row + "-" + Grid[i][j].col + " ")
@@ -28,6 +32,17 @@ Grid[8][8].State = "End"
 var StartPoint = [0,0]
 var EndPoint = [GRID_ROW_SIZE -1, GRID_COL_SIZE -1]
 var setterMode="Wall"
+var mouseDown = false
+
+this.addEventListener("mousedown", e=>{
+    console.log("MouseDown")
+    mouseDown = true;
+})
+this.addEventListener("mouseup", e=>{
+    console.log("MouseUp")
+    mouseDown = false;
+})
+
 
 ////////////////// END OF THE DATA SEGMENT ///////////////////////////////////////
 
@@ -73,16 +88,15 @@ function clearGrid(){
         for (let j = 0; j < GRID_COL_SIZE; j++) {
             Grid[i][j].State = "NULL"
             Grid[i][j].VisitedAt = -1
-            var nodeName="node"+i+"x"+j
-            document.getElementById(nodeName).innerHTML="";
+            document.getElementById(Grid[i][j].id).innerHTML="";
     
         }
     }
     Grid[0][0].State = "Start"
     Grid[GRID_ROW_SIZE-1][GRID_COL_SIZE-1].State = "End"
-    document.getElementById("node"+0+"x"+0).innerHTML="<div class='startpoint'></div>";
-    document.getElementById("node"+(GRID_ROW_SIZE-1)+"x"+(GRID_ROW_SIZE-1)).innerHTML="<div class='endpoint'></div>";
-
+    document.getElementById(Grid[0][0].id).innerHTML="<div class='startpoint'></div>";
+    document.getElementById(Grid[GRID_ROW_SIZE-1][GRID_COL_SIZE-1].id).innerHTML="<div class='endpoint'></div>";
+    
 }
 
 /**
@@ -92,11 +106,12 @@ function clearGridSaveWall(){
     for (let i = 0; i < GRID_ROW_SIZE; i++) {    
         for (let j = 0; j < GRID_COL_SIZE; j++) {
             let State = Grid[i][j].State;
-            if(status == "Wall" || status == "Start" || status == "End"){
+            if(State == "Wall" || State == "Start" || State == "End"){
                 continue;
             }
             Grid[i][j].State = "NULL"
             Grid[i][j].VisitedAt = -1
+            document.getElementById(Grid[i][j].id).innerHTML="";
         }
     }
     
@@ -118,10 +133,14 @@ function setStart(rownum,colnum){
         return
     }
     Grid[StartPoint[0]][StartPoint[1]].State = "NULL"
+    document.getElementById(Grid[StartPoint[0]][StartPoint[1]].id).innerHTML="";
+
     Grid[rownum][colnum].State = "Start"
+    document.getElementById(Grid[rownum][colnum].id).innerHTML="<div class='"+setterMode+"'></div>";
     StartPoint = [rownum,colnum]
-    var nodeName="node"+rownum+"x"+colnum
-    document.getElementById(nodeName).innerHTML="<div class='"+setterMode+"'></div>";
+
+
+
     return
 
 }
@@ -142,10 +161,11 @@ function setEnd(rownum,colnum){
         return
     }
     Grid[EndPoint[0]][EndPoint[1]].State = "NULL"
+    document.getElementById(Grid[EndPoint[0]][EndPoint[1]].id).innerHTML="";
+
     Grid[rownum][colnum].State = "End"
+    document.getElementById(Grid[rownum][colnum].id).innerHTML="<div class='"+setterMode+"'></div>";
     EndPoint = [rownum,colnum]
-    var nodeName="node"+rownum+"x"+colnum
-    document.getElementById(nodeName).innerHTML="<div class='"+setterMode+"'></div>";
     return
 
 }
@@ -170,8 +190,7 @@ function setWall(rownum,colnum){
         return
     }
     Grid[rownum][colnum].State = "Wall"
-    var nodeName="node"+rownum+"x"+colnum
-    document.getElementById(nodeName).innerHTML="<div class='"+setterMode+"'></div>";
+    document.getElementById(Grid[rownum][colnum].id).innerHTML="<div class='"+setterMode+"'></div>";
     console.log("setWall")
     // document.getElementById(nodeName).style.backgroundColor="black";
     return
@@ -195,6 +214,7 @@ function resetVisit(){
  * or show message that a path cannot be reached
  */
 function BFS(){
+    clearGridSaveWall()
     resetVisit()
     //setting the startpoint to 0 to initiate the BFS
     Grid[StartPoint[0]][StartPoint[1]].VisitedAt = 0;
@@ -227,48 +247,52 @@ function BFS(){
                     if(Grid[i-1][j].VisitedAt == roundIndicator-1){
                         modifiedThisRound = true
                         Grid[i][j].VisitedAt = roundIndicator
-                        // document.getElementById("node"+i+"x"+j).innerHTML="<div id='explored"+i+"x"+j+"' class='explored'></div>";
-                        // document.getElementById("explored"+i+"x"+j).style.animationDelay=roundIndicator/5+"s"
                         if(i == EndPoint[0] && j == EndPoint[1]){
                             found = true
                             break;
                         }
+                        document.getElementById(Grid[i][j].id).innerHTML = "<div id='explored"+i+"x"+j+"' class='explored'></div>"
+                        document.getElementById("explored"+i+"x"+j).style.animationDelay=roundIndicator/5+"s"
+                        
                     }
                 }
                 if(i<GRID_ROW_SIZE-1){
                     if(Grid[i+1][j].VisitedAt == roundIndicator-1){
                         modifiedThisRound = true
                         Grid[i][j].VisitedAt = roundIndicator
-                        // document.getElementById("node"+i+"x"+j).innerHTML="<div id='explored"+i+"x"+j+"' class='explored'></div>";
-                        // document.getElementById("explored"+i+"x"+j).style.animationDelay=roundIndicator/5+"s"
                         if(i == EndPoint[0] && j == EndPoint[1]){
                             found = true
                             break;
                         }
+                        document.getElementById(Grid[i][j].id).innerHTML = "<div id='explored"+i+"x"+j+"' class='explored'></div>"
+                        document.getElementById("explored"+i+"x"+j).style.animationDelay=roundIndicator/5+"s"
+                        
                     }
                 }
                 if(j>0){
                     if(Grid[i][j-1].VisitedAt == roundIndicator-1){
                         modifiedThisRound = true
                         Grid[i][j].VisitedAt = roundIndicator
-                        // document.getElementById("node"+i+"x"+j).innerHTML="<div id='explored"+i+"x"+j+"' class='explored'></div>";
-                        // document.getElementById("explored"+i+"x"+j).style.animationDelay=roundIndicator/5+"s"
+                        
                         if(i == EndPoint[0] && j == EndPoint[1]){
                             found = true
                             break;
                         }
+                        document.getElementById(Grid[i][j].id).innerHTML = "<div id='explored"+i+"x"+j+"' class='explored'></div>"
+                        document.getElementById("explored"+i+"x"+j).style.animationDelay=roundIndicator/5+"s"
                     }
                 }
                 if(j<GRID_COL_SIZE-1){
                     if(Grid[i][j+1].VisitedAt == roundIndicator-1){
                         modifiedThisRound = true
                         Grid[i][j].VisitedAt = roundIndicator
-                        // document.getElementById("node"+i+"x"+j).innerHTML="<div id='explored"+i+"x"+j+"' class='explored'></div>";
-                        // document.getElementById("explored"+i+"x"+j).style.animationDelay=roundIndicator/5+"s"
                         if(i == EndPoint[0] && j == EndPoint[1]){
                             found = true
                             break;
                         }
+                        document.getElementById(Grid[i][j].id).innerHTML = "<div id='explored"+i+"x"+j+"' class='explored'></div>"
+                        document.getElementById("explored"+i+"x"+j).style.animationDelay=roundIndicator/5+"s"
+                        
                     }
                 }
 
@@ -287,7 +311,20 @@ function BFS(){
         return
     }
     console.log("Found at round: " + roundIndicator)
-    pathGatherer()
+
+
+    var counter = 0
+    var myInterval = setInterval(() => {
+        if(counter < 1000*roundIndicator/5){
+            counter+= 100
+        }
+        else{
+            pathGatherer()
+            clearInterval(myInterval)
+        }
+    }, 100);
+
+
 
 }
 
@@ -299,15 +336,13 @@ function pathGatherer(){
     var currentPoint = [EndPoint[0],EndPoint[1]]
     var path = []
 
-
-
     while( indicator != 0){
         console.log(indicator)
         console.log("Current: " + currentPoint[0] + "-" + currentPoint[1])
         if(currentPoint[0]!=EndPoint[0] | currentPoint[1]!=EndPoint[1]){
             console.log(currentPoint,"not equal to",EndPoint)
-            document.getElementById("node"+currentPoint[0]+"x"+currentPoint[1]).innerHTML="<div id='path"+currentPoint[0]+"x"+currentPoint[1]+"' class='path'></div>";
-            document.getElementById("path"+currentPoint[0]+"x"+currentPoint[1]).style.animationDelay=Grid[currentPoint[0]][currentPoint[1]].VisitedAt/15+"s"
+            document.getElementById(Grid[currentPoint[0]][currentPoint[1]].id).innerHTML="<div id='path"+currentPoint[0]+"x"+currentPoint[1]+"' class='path'></div>";
+            document.getElementById("path"+currentPoint[0]+"x"+currentPoint[1]).style.animationDelay=indicator/15+"s"
         }
         
         Grid[currentPoint[0]][currentPoint[1]].State = "Path"
@@ -395,7 +430,7 @@ function initializeGraph(){
     var tempHTML=""
     for (let i = 0; i < GRID_ROW_SIZE; i++) {    
         for (let j = 0; j < GRID_COL_SIZE; j++) {
-            tempHTML+="<node id='node"+i+"x"+j+"' data-row='"+i+"' data-column='"+j+"'></node>"
+            tempHTML+="<node id=\"" + Grid[i][j].id + "\" data-row=\"" + i + "\" data-column=\""+ j + "\"></node>"
         }
         tempHTML+="<br></br>"
     }
@@ -403,12 +438,29 @@ function initializeGraph(){
     document.getElementById("graphWrapper").innerHTML=tempHTML;
     for (let i = 0; i < GRID_ROW_SIZE; i++) {    
         for (let j = 0; j < GRID_COL_SIZE; j++) {
-            document.getElementById("node"+i+"x"+j).addEventListener("click", setNode)
+            document.getElementById(Grid[i][j].id).addEventListener("click", setNode)
+            document.getElementById(Grid[i][j].id).addEventListener("mouseover",toggleRedirect)
+            document.getElementById(Grid[i][j].id).addEventListener("mouseup",e=>{
+                console.log("MouseUp")
+                mouseDown = false;
+            })
         }
     }
-    document.getElementById("node"+StartPoint[0]+"x"+StartPoint[1]).innerHTML="<div class='startpoint'></div>";
-    document.getElementById("node"+EndPoint[0]+"x"+EndPoint[1]).innerHTML="<div class='endpoint'></div>";
+    document.getElementById(Grid[0][0].id).innerHTML="<div class='startpoint'></div>";
+    document.getElementById(Grid[GRID_ROW_SIZE-1][GRID_COL_SIZE-1].id).innerHTML="<div class='endpoint'></div>";
 }
+
+/**
+ * redirect the mousedown and over event
+ * @param  event 
+ */
+function toggleRedirect(event){
+    if(mouseDown){
+        setNode(event)
+    }
+}
+
+
 
 /**
  * set the node to the state we want
@@ -418,19 +470,15 @@ function setNode(event){
     
     let indexX=parseInt(currentNode.dataset.row)
     let indexY=parseInt(currentNode.dataset.column)
-    if(Grid[indexX][indexY].State==setterMode){
-        return
+    
+    if(setterMode=="Wall"){
+        setWall(indexX,indexY)
     }
-    else{
-        if(setterMode=="Wall"){
-            setWall(indexX,indexY)
-        }
-        else if(setterMode=="StartPoint"){
-            setStart(indexX,indexY)
-        }
-        else if(setterMode=="EndPoint"){
-            setEnd(indexX,indexY)
-        }
+    else if(setterMode=="StartPoint"){
+        setStart(indexX,indexY)
+    }
+    else if(setterMode=="EndPoint"){
+        setEnd(indexX,indexY)
     }
         
 }
